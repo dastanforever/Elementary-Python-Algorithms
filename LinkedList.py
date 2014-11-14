6ef992e2b94ecf6b6ad17a668b596f5059695332
Pranav Sharma <professionalpranav@outlook.com>
2014-11-14 11:11:04 -0800

    LinkedList Using Python
    Linked List insertion, deletion, and traversal added using python

---
+++ LinkedList.py
@@ -0,0 +1,57 @@
+__author__ = 'Pranav Sharma'
+
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+        self.prev = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+ll = Linkedlist()
+
+def insert(data):
+    x = node()
+    x.data = data
+    if ll.head == None:
+        ll.head = node()
+        ll.head = x
+    else:
+        x.prev = ll.head
+        ll.head.next = x
+        ll.head = x
+
+def delete(data):
+    temp = ll.head
+    while temp.prev != None and temp.prev.data != data:
+        temp = temp.prev
+    if temp.prev != None and temp.prev.data == data:
+        temp.prev = temp.prev.prev
+    else:
+        print("No Item Found!!!!")
+
+def printlist():
+    temp = ll.head
+    while temp.prev != None:
+        print(temp.data)
+        temp = temp.prev
+    print(temp.data)
+
+
+i = 1
+while i == 1:
+    print("Enter - ")
+    data = int(raw_input())
+    insert(data)
+    print("Inserted!!Continue ? ")
+    #print(data)
+    i = int(raw_input())
+
+#print head.prev.data
+#head.next.data
+printlist()
+delete(3)
+printlist()
