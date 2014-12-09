8dd21bfb1903833da4cb3a5e6e248a2deacf15f0
Pranav Sharma <professionalpranav@outlook.com>
2014-12-08 17:01:46 -0800

    Fermat Primality Test
    Using fermat's discovery, a simple implementation.

---
+++ fermatPrimality.py
@@ -0,0 +1,17 @@
+from random import sample
+
+def check(a):
+	s = sample(range(0,2**10),100)
+	for x in s:
+		if (x**(a-1) - 1) % a != 0:
+			print 'Not prime!!'
+			return
+	print "Prime"
+	return
+
+
+def main():
+	check(3147)
+
+if __name__ == "__main__":
+	main()
